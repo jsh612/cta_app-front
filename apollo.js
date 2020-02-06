a94d4078af322c05967f1de993b99af60d639637
seungho Jeon <urijsh612@gmail.com>
2020-02-06 22:16:22 +0900

    uri 변경

--- apollo.js
+++ apollo.js
@@ -1,9 +1,9 @@
 import { AsyncStorage } from "react-native";
-import { uris } from "./ip";
+import { uri } from "./ip";
 
 // ApolloClient 옵션 설정
 const apolloClientOptions = {
-  uri: uris,
+  uri: uri,
   //requet = 매 요청시 미다 실행
   request: async operation => {
     //매 요청마다 토큰을 header에 전달하여 유저 인증이 되었음을 보여준다
